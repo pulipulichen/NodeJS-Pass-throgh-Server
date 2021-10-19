f1c1dbb0e12d7cb5ecf55fa10de94b91469d9578
Pulipuli Chen <blog@pulipuli.info>
2021-10-20 02:20:06 +0800

    1020 完美演繹，接下來就只剩下上NAS了

---
+++ mount.example/route.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    uri: '/blog/',
+    backend: 'https://blog.pulipuli.info/'
+  },
+  {
+    uri: '303',
+    backend: 'http://pulipuli.myqnapcloud.com:30380/'
+  },
+  {
+    uri: '304',
+    backend: 'http://localhost:3000/'
+  }
+]

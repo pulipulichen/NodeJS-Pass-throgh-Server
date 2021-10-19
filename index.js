c28b48508308de4a8675b898e6bd66ac8318f6c3
Pulipuli Chen <blog@pulipuli.info>
2021-10-20 02:56:41 +0800

    1020 完美演繹，接下來就只剩下上NAS了

--- index.js
+++ index.js
@@ -8,26 +8,31 @@ const config = require('./mount/config.js')
 const route = require('./app/RouteLoader.js')
 
 http.createServer(function (req, res) {
-  for (let i = 0; i < route.length; i++) {
-    let {uri, backend} = route[i]
-    
-    if (req.url.startsWith(uri)) {
-        let requestURI = req.url.slice(uri.length)
-        
-      let requestURL = backend + requestURI
+  try {
+    for (let i = 0; i < route.length; i++) {
+      let {uri, backend} = route[i]
+
+      if (req.url.startsWith(uri)) {
+          let requestURI = req.url.slice(uri.length)
+
+        let requestURL = backend + requestURI
+        //console.log(requestURL)
+        req.pipe(request(requestURL)).pipe(res)
+        return true
+      }
+    }
+
+    if (config.enablePound) {
+
+      let uri = req.url.slice(1)
+      let baseOrigin = 'http://127.0.0.1:8080/';
+      let requestURL = baseOrigin + uri
       //console.log(requestURL)
       req.pipe(request(requestURL)).pipe(res)
-      return true
     }
   }
-  
-  if (config.enablePound) {
-    
-    let uri = req.url.slice(1)
-    let baseOrigin = 'http://127.0.0.1:8080/';
-    let requestURL = baseOrigin + uri
-    //console.log(requestURL)
-    req.pipe(request(requestURL)).pipe(res)
+  catch (e) {
+    console.error(e)
   }
 }).listen(80);
 
